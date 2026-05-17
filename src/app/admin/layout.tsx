b81814488f8e26d7f5e8527473ddd6221e34cbb2
raaamooo <omaribrahem1050@gmail.com>
2026-05-17 23:23:28 +0300

    admin pages fixed

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import AdminHeader from '@/components/layout/AdminHeader';
+import './Admin.css';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return <AdminHeader>{children}</AdminHeader>;
 }
+
