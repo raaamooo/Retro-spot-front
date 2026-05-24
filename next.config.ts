a03cbeef5176e08c81756b8d11c03cd5fb55b4af
raaamooo <omaribrahem1050@gmail.com>
2026-05-25 02:52:18 +0300

    fixes for vercel

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,17 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
 
+  // ── Redirect everything to Vercel ──────────────────────────────
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: `https://retro-spot-front.vercel.app/:path*`,
+        permanent: true, // 301
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
